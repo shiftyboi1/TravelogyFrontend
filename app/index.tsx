7a3301aae54d55d92259d051a7a49b57d2a64622
shiftyboi1 <drugzarbest@gmail.com>
2026-01-01 11:49:57 +0100

    Move around files like a lunatic, add light and dark mode support

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
-import { View } from "react-native";
+import { Colors } from "@/constants/theme";
+import { useColorScheme, View } from "react-native";
 
 export default function Index() {
+  const colorScheme = useColorScheme();
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: Colors[colorScheme ?? "light"].background,
       }}
     >
     </View>
