1132763a013721b5ed2249dba6d9eedbdc4f3e35
shift <drugzarbest@gmail.com>
2026-01-13 18:41:26 +0100

    commit

--- features/search/components/custom-switch.tsx
+++ features/search/components/custom-switch.tsx
@@ -1,9 +1,12 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { StyleSheet, View } from "react-native";
+
 export type CustomSwitchProps = {
   values: {
     label: string;
     value?: string;
   }[]
-  bgColor?: {
+  backgroundColor?: {
     light: string;
     dark: string;
   }
@@ -13,3 +16,28 @@ export type CustomSwitchProps = {
   }
 }
 
+export const CustomSwitch = ({
+  values,
+  backgroundColor,
+  selectionColor,
+}: CustomSwitchProps) => {
+  const bgColor = useThemeColor({ light: backgroundColor?.light, dark: backgroundColor?.dark }, "background");
+  const selectColor = useThemeColor({ light: selectionColor?.light, dark: selectionColor?.dark }, "secondary");
+  return (
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      {/* TODO: Map values  */}
+      {/* TODO: Handle selection  */}
+      {/* TODO: Between lines  */}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 4,
+    borderRadius: 16,
+  }
+});
