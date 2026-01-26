a018e14cae98d74036b80c524bfa72bb64b4e807
shiftyboi1 <drugzarbest@gmail.com>
2026-01-26 14:15:48 +0100

    Begin implementing file API

---
+++ services/save-file-system.ts
@@ -0,0 +1,59 @@
+import { Directory, File, Paths } from 'expo-file-system';
+
+export enum DataKey {
+  TAGS = 'tags',
+  TYPES = 'types',
+}
+
+export enum StorageLocation {
+  DATA =  'data',
+  DOCUMENTS = 'docs',
+}
+
+const rootDir = new Directory(Paths.document, 'travelogy_v2');
+
+export class SaveFileSystem {
+
+  private static getDirectory(location: StorageLocation): Directory {
+    return new Directory(rootDir, location);
+  }
+
+  static async ensureDirExists() {
+    if (!rootDir.exists) {
+      await rootDir.create();
+    }
+  }
+
+  static async set<T>(location: StorageLocation, key: DataKey, data: T): Promise<void> {
+    try {
+      await this.ensureDirExists();
+      const file = new File(this.getDirectory(location), `${key}.json`);
+      await file.write(JSON.stringify({ data }));
+    } catch (error) {
+      console.error(`[SaveFile] Write failed for ${key}:`, error);
+    }
+  }
+
+  static async get<T>(location: StorageLocation, key: DataKey): Promise<T | null> {
+    try {
+      const file = new File(this.getDirectory(location), `${key}.json`);
+
+      if (!file.exists) return null;
+
+      return JSON.parse(await file.text()).data as T; 
+    } catch (error) {
+       console.warn(`[FileSystemCache] Read failed for ${key}`, error);
+      return null;
+    }
+  }
+
+  static async remove(location: StorageLocation, key: DataKey) {
+    const file = new File(this.getDirectory(location), `${key}.json`);
+    if (file.exists) {
+      await file.delete();
+    }
+  }
+
+  // TODO: Fetch all content from file
+  // TODO: Remove test warnings
+}
