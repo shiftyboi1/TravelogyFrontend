1c0ccf15bc308de4c78630543bce10e6443fc217
shift <drugzarbest@gmail.com>
2026-02-13 13:42:43 +0100

    Fix typo

--- api/client.ts
+++ api/client.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://127.0.0.1/8080";
+const BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://127.0.0.1:8080";
 
 export async function client<T>(endpoint: string, options?: RequestInit & { body?: string }): Promise<T> {
   const defaults = {
